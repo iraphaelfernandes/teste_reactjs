1e2795a096e565d2117e62ad964744cceb158939
Raphael Fernandes <raphael.fernandes.eng@gmail.com>
2020-02-02 12:59:36 -0300

    For up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Mybutton extends Component{
+  render(){
+    return <button
+    onClick={() =>{this.props.handleClick(this.props.label);}}
+    >
+    {this.props.label}
+    </button>
+
+  }
+}
+/*todo componente tem o this.props e o this.state 
+ *tudo que você passa como parâmetro para o component é dito como this.props
+ *O state representa o estado, simples assim.
+*/
+class MyLabel extends Component{
+  render(){
+    return(
+        <p>Click no: {this.props.text}</p> 
+    )
+  }
 }
 
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      labelText: '',
+    }
+  }
+
+  setLabelText = (labelText) =>{
+    this.setState({labelText});
+    
+  }
+  render(){
+    return(
+    <div className="App">
+      <MyLabel text={this.state.labelText}/>
+      <Mybutton handleClick={this.setLabelText} label="Botão 1"/>
+      <Mybutton handleClick={this.setLabelText} label="Botão 2"/>
+      <Mybutton handleClick={this.setLabelText} label="Botão 3"/>
+      <Mybutton handleClick={this.setLabelText} label="Botão 4"/>
+    </div>)
+  }
+}
 export default App;
